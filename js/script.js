219416fe9028e80ceffbce4e484a22fe786a47ad
someshfengde <42097653+someshfengde@users.noreply.github.com>
2020-07-21 18:34:47 +0530

    added templet

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// console.log(getElementsById("button"))
+
+
+console.log("this is this")
+function sayit(event){
+	console.log(event);
+var name = document.getElementsById("name").value;
+
+}
+var me = "Coursera";
+document.getElementsById("Hello").addEventListner('Click',function(){
+	document.getElementsById('Hello').innerHTML = "hello"+"Coursera";
+})
+
